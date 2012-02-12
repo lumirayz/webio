54b0dbc090e2f4e0ee8e23d4e8815aa7bd690d95
lumirayz <lumirayz@gmail.com>
2012-02-12 18:31:43 +0100

    typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 	setup(
 		name = "webio",
 		author = "lumirayz",
-		autor_email = "lumirayz@gmail.com",
+		author_email = "lumirayz@gmail.com",
 		description = "a server push mechanism using long polling",
 		version = "1.0",
 		py_modules = ["webio"]
